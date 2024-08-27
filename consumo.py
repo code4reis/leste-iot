01a658878bf148801c4bc8d95bc16e52e91799ef
victorhugossantos <victorhugossantos@gmail.com>
2024-08-27 20:36:17 -0300

    Ajustes na exibicao do consumo de dados

--- consumo.py
+++ consumo.py
@@ -1,8 +1,28 @@
 from flask import render_template, Blueprint
+import os, json
 
 
 app = Blueprint('consumo', __name__)
 
+
+RESULTS_FILE = 'consumo.json'
+
+# funcao para carregar os resultados do json
+
+def load_results():
+    if os.path.exists(RESULTS_FILE):
+        with open (RESULTS_FILE, 'r') as file:
+            try: 
+                data = json.load(file)
+                print('Dados carregados: ', data)
+                return data
+            except json.JSONDecodeError:
+                print('Algum erro ocorreu')
+                return [] # se o arquivo estiver corrompido ou vazio
+
 @app.route('/consumo')
-def speedtest():
-    return render_template('consumo.html')
+def consumo():
+    items = load_results()
+    print('Dados carregados para o template: ', items)
+    return render_template('consumo.html', items = items)
+ 
