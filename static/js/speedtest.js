6ffea296b6e6a762300683e172f3fd8fe76588f0
Super User <root@speed-teste-iot.186.211.32.98>
2024-08-28 23:11:01 +0000

    v1.0.3

--- static/js/speedtest.js
+++ static/js/speedtest.js
@@ -12,56 +12,35 @@ async function startSpeedTest() {
     button.disabled = true; // Desativa o botão
 
     try {
-        for (let i = 0; i < 5; i++) {
-            const startResponse = await fetch(`${agentUrl}/start_speedtest`, { method: 'POST' });
-            if (!startResponse.ok) {
-                console.error('Erro ao iniciar teste de velocidade');
-                button.textContent = 'Iniciar Teste de Velocidade';
-                button.disabled = false; // Reativa o botão
-                return;
-            }
-            console.log('Speed test started');
+        const response = await fetch(`${agentUrl}/start_speedtest`, { method: 'GET' });
+        const data = await response.json();
 
-            let testCompleted = false;
-            while (!testCompleted) {
-                const response = await fetch(`${agentUrl}/speedtest`, { method: 'GET'});
-                const data = await response.json();
+        if (response.ok && data.download_speed_mbps) {
+            document.getElementById("download_speed").textContent = Math.round(data.download_speed_mbps);
+            updateSpeedometer(Math.round(data.download_speed_mbps), "needle");
+            document.getElementById("upload_speed").textContent = Math.round(data.upload_speed_mbps);
+            document.getElementById("ping_result").textContent = data.ping_ms;
+            document.getElementById("server_result").textContent = data.server.sponsor;
 
-                if (response.ok && data.download_speed_mbps) {
-                    document.getElementById("download_speed").textContent = Math.round(data.download_speed_mbps);
-                    updateSpeedometer(Math.round(data.download_speed_mbps), "needle");
-                    document.getElementById("upload_speed").textContent = Math.round(data.upload_speed_mbps);
-                    document.getElementById("ping_result").textContent = data.ping_ms;
-                    document.getElementById("server_result").textContent = data.server.sponsor;
-
-                      // Enviar dados para o servidor Flask
-                      await fetch('http://192.168.100.107:5000/save_speedteste', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            // Convertendo valores para o tipo desejado
-                            download_speed_mbps: Math.floor(data.download_speed_mbps),  // Exemplo de conversão para número
-                            upload_speed_mbps: Math.floor(data.upload_speed_mbps),      // Exemplo de conversão para número
-                            ping_ms: Math.floor(data.ping_ms),                        // Convertendo para inteiro
-                            server: data.server.sponsor.toString()                    // Convertendo para string
-                        })
-                    });
-
-                    testCompleted = true; // Marca o teste como concluído
-                } else if (data.error) {
-                    console.error(`Erro: ${data.error}`);
-                    break; // Encerra o loop em caso de erro
-                } else {
-                    console.log('Teste em andamento... aguardando...');
-                }
-
-                await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 2 segundos antes de tentar novamente
-            }
-
-            await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 5 segundos antes de iniciar o próximo teste
+            // Enviar dados para o servidor Flask
+            await fetch('http://192.168.100.107:5000/save_speedteste', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    download_speed_mbps: Math.floor(data.download_speed_mbps),  // Exemplo de conversão para número
+                    upload_speed_mbps: Math.floor(data.upload_speed_mbps),      // Exemplo de conversão para número
+                    ping_ms: Math.floor(data.ping_ms),                          // Convertendo para inteiro
+                    server: data.server.sponsor.toString()                      // Convertendo para string
+                })
+            });
+        } else if (data.error) {
+            console.error(`Erro: ${data.error}`);
+        } else {
+            console.log('Nenhum dado de velocidade foi retornado.');
         }
+
     } catch (error) {
         console.error('Erro ao buscar velocidade:', error);
     }
